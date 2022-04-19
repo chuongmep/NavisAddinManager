b223e81c1301221252c53b605ca7f61677c757f9
chuongmep <chuongpqvn@gmail.com>
2022-04-19 23:31:42 +0700

    bump 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 # Changelog
+- 2022-04-20 **1.0.1**
+  - Add panel test lab to try load assembly
 - 2022-04-19 **1.0.0**
   - First Release.
   - Support Navis Version : 2020,2021,2022,2023
--- NavisAddinManager/App.cs
+++ NavisAddinManager/App.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Navisworks.Api.Plugins;
 using NavisAddinManager.Command;
 using NavisAddinManager.Properties;
+using Test;
 
 namespace NavisAddinManager;
 
@@ -9,6 +10,7 @@ namespace NavisAddinManager;
 [RibbonTab("ID_AddinManager_TAB",DisplayName = "AddinManager")]
 [Command("ID_ButtonAddinManagerManual", DisplayName = "Addin Manager \n Manual", Icon = "Resources\\dev16x16.png", LargeIcon = "Resources\\dev32x32.png",ToolTip = "Addin Manager Manual")]
 [Command("ID_ButtonAddinManagerFaceless", DisplayName = "Addin Manager \n Faceless",Icon = "Resources\\dev16x16.png", LargeIcon = "Resources\\dev32x32.png", ToolTip = "Addin Manager Faceless")]
+[Command("ID_ButtonTest", DisplayName = "Test",Icon = "Resources\\lab16x16.png", LargeIcon = "Resources\\lab32x32.png", ToolTip = "Test")]
 public class App  : CommandHandlerPlugin
 {
     public override int ExecuteCommand(string name, params string[] parameters)
@@ -22,6 +24,10 @@ public class App  : CommandHandlerPlugin
             case "ID_ButtonAddinManagerFaceless":
                 AddInManagerFaceLess addInManagerFaceless = new AddInManagerFaceLess();
                 addInManagerFaceless.Execute();
+                break;
+            case "ID_ButtonTest":
+                TestAddInManagerManual hello = new TestAddInManagerManual();
+                hello.Execute();
                 break;
         }
         return 0;
