f300bee7681229bdf5292f7f3e14ab30e4e2789c
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-04-22 17:02:49 +0700

    test zoom to

---
+++ Test/ZoomToCurrentSelection.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+using ComApi = Autodesk.Navisworks.Api.Interop.ComApi;
+using ComApiBridge = Autodesk.Navisworks.Api.ComApi;
+
+namespace Test;
+
+public class ZoomToCurrentSelection : AddInPlugin
+{
+    public override int Execute(params string[] parameters)
+    {
+        ZoomTo();
+        return 0;
+    }
+
+    public void ZoomTo()
+
+    {
+        ComApi.InwOpState10 comState = ComApiBridge.ComApiBridge.State;
+
+        //Create a collection
+
+        ModelItemCollection modelItemCollectionIn =
+            new ModelItemCollection(Application.ActiveDocument.CurrentSelection.SelectedItems);
+
+        ComApi.InwOpSelection comSelectionOut = ComApiBridge.ComApiBridge.ToInwOpSelection(modelItemCollectionIn);
+
+        // zoom in to the specified selection
+
+        comState.ZoomInCurViewOnSel(comSelectionOut);
+
+        // zoom in to the current selection
+
+        //comState.ZoomInCurViewOnCurSel();
+    }
+}
