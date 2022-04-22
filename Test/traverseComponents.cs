4a499baa02d16edebb0db99beafdf7516c90db36
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-04-22 16:13:27 +0700

    test traverseComponents

---
+++ Test/traverseComponents.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace Test;
+
+public class traverseComponents  :  AddInPlugin
+{
+    public override int Execute(params string[] parameters)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (ModelItem item in
+
+                 Autodesk.Navisworks.Api.Application
+
+                     .ActiveDocument.Models.RootItemDescendantsAndSelf)
+        {
+
+            sb.Append(
+
+                "Display Name: " + item.DisplayName +
+
+                "; Class Display Name: " + item.ClassDisplayName +
+
+                "\n");
+
+        }
+
+        string tempPath = Path.GetTempPath();
+        string path  = Path.Combine(tempPath, "test.txt");
+        using (StreamWriter streamWriter = new StreamWriter(path))
+        {
+            streamWriter.Write(sb.ToString());
+            streamWriter.Close();
+        }
+        MessageBox.Show(sb.Length.ToString());
+        Process.Start(path);
+        return 0;
+    }
+
+}
