7d508a9b9970c39aa0fca60dc5000ef6aa6bda78
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-04-22 16:22:18 +0700

    test SetHidden

---
+++ Test/SetHidden.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace Test;
+
+public class SetHidden : AddInPlugin
+{
+    public override int Execute(params string[] parameters)
+    {
+        // Create hidden collection
+
+        ModelItemCollection hidden = new ModelItemCollection();
+
+        // Add all the items that are visible to visible collection
+
+        hidden.AddRange(Autodesk.Navisworks.Api.Application.ActiveDocument.CurrentSelection.SelectedItems);
+        // hide the remaining items
+
+        Autodesk.Navisworks.Api.Application.ActiveDocument.Models.SetHidden(hidden, true);
+        return 0;
+    }
+}
